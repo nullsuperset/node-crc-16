39fbe6f3cb524daffa120d9c43b6dded85e136f2
nullsuperset <null.superset@enhack.com>
2016-06-26 02:14:45 -0400

    removed buffer constructor from function for compatibility

--- test/crc-16.test.js
+++ test/crc-16.test.js
@@ -8,17 +8,11 @@ var crc16 = require('../lib/crc-16');
 var expect = chai.expect;
 
 
-// mocha and chai need to be installed
-describe('Mocha & Chai', function () {
-  it('should have been installed and running for fluent testing', function () {
-    expect(true).to.deep.equal(true);
-  });
-});
-
 // simple 2 byte CRC calculation test
 describe('CRC-16', function () {
   it('should have returned [ 0x47 , 0x0f ] given the input [ 0x00 , 0x00 ]', function () {
-    var crc = new Buffer(crc16(new Buffer([0x00, 0x00])));
-    expect(crc.compare(new Buffer([0x47, 0x0f]))).to.deep.equal(0);
+    const buf = new Buffer([0,0]);
+    var crc = new Buffer(crc16(buf));
+    expect(crc.equals(new Buffer([0x47, 0x0f]))).is.true;
   });
 });
